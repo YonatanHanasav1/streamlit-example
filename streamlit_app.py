8bcee294b5add5d0fc0f8e26b235a743ca5d4048
YonatanHanasav1 <149471467+YonatanHanasav1@users.noreply.github.com>
2023-12-19 11:28:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,76 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+ uploaded_file = st.file_uploader("Choose a file", type=["csv"])
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    if uploaded_file:
+        if str(uploaded_file.name)[-3:] == 'csv':
+            df = pd.read_csv(uploaded_file, low_memory=False) # by default read first sheet of the file
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+df.head()
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+checked_column = 'labor_duration'
+df=df[df[checked_column]!=0]
+df[checked_column]=df[checked_column].astype(float)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+def histogrammer(column_str, field, data, median_text=True, **kwargs):
+    if field:
+        filtered_data = data[data['event_type'] == 'field']
+        event_type_label = 'field'
+    else:
+        filtered_data = data[data['event_type'] == 'remote']
+        event_type_label = 'remote'
+    
+    median = round(filtered_data[column_str].median(), 1)
+    plt.figure(figsize=(8, 5))
+    ax = sns.histplot(x=filtered_data[column_str], **kwargs)
+    plt.axvline(median, color='red', linestyle='--')
+    if median_text:
+        ax.text(0.25, 0.85, f'median={median}', color='red',
+                ha="left", va="top", transform=ax.transAxes)
+    else:
+        print('Median:', median)
+    plt.xlabel(f'{event_type_label}_{column_str}')  # Add this line to include the x-axis label
+    plt.show()
+def boxplotter(column_str, field, data):
+    if field:
+        filtered_data = data[data['event_type'] == 'field']
+        event_type_label = 'field'
+    else:
+        filtered_data = data[data['event_type'] == 'remote']
+        event_type_label = 'remote'
+    
+    median = round(filtered_data[column_str].median(), 1)
+    
+    plt.figure(figsize=(10, 2))
+    ax = sns.boxplot(x=filtered_data[column_str], fliersize=1)
+    
+    plt.xlabel(f'{event_type_label}_{column_str}')  # Add this line to include the x-axis label
+    
+    ax.text(0.25, 0.85, f'median={median}', color='red',
+            ha="left", va="top", transform=ax.transAxes)
+    
+    plt.show()
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+# Calculate 25th percentile of annual strikes
+percentile25 = df[checked_column].quantile(0.25)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# Calculate 75th percentile of annual strikes
+percentile75 = df[checked_column].quantile(0.75)
+
+# Calculate interquartile range
+iqr = percentile75 - percentile25
+
+# Calculate upper and lower thresholds for outliers
+upper_limit = percentile75 + 1.5 * iqr
+lower_limit = percentile25 - 1.5 * iqr
+
+print('Lower limit is: ', lower_limit)
+print('Upper limit is: ', upper_limit)
+
+histogrammer(checked_column,field=True,data = df)
+
+boxplotter(checked_column,field=True,data = df)
