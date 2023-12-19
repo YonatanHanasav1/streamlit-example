92cd0a8becfca06faf8467974e671340d1135a10
YonatanHanasav1 <149471467+YonatanHanasav1@users.noreply.github.com>
2023-12-19 14:36:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,26 +5,6 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
-def histogrammer(column_str, field, data, median_text=True):
-    if field:
-        filtered_data = data[data['event_type'] == 'field']
-        event_type_label = 'field'
-    else:
-        filtered_data = data[data['event_type'] == 'remote']
-        event_type_label = 'remote'
-    
-    median = round(filtered_data[column_str].median(), 1)
-    plt.figure(figsize=(8, 5))
-    ax = sns.histplot(x=filtered_data[column_str],bins=20, color='blue')
-    plt.axvline(median, color='red', linestyle='--')
-    if median_text:
-        ax.text(0.25, 0.85, f'median={median}', color='red',
-                ha="left", va="top", transform=ax.transAxes)
-    else:
-        print('Median:', median)
-    plt.xlabel(f'{event_type_label}_{column_str}')  # Add this line to include the x-axis label
-    plt.show()
-
 def boxplotter(column_str, field, data):
     if field:
         filtered_data = data[data['event_type'] == 'field']
@@ -46,7 +26,8 @@ def boxplotter(column_str, field, data):
         y=filtered_data[column_str],
         boxpoints="outliers",  # Show only outliers
         marker=dict(color='blue'),
-        line=dict(color='blue')
+        line=dict(color='blue'),
+        hovertext=filtered_data[column_str].tolist()  # Set hover text to the values of labor_duration
     ))
 
     # Add a red line for the median
@@ -79,5 +60,3 @@ if uploaded_file:
     df = pd.read_csv(uploaded_file, low_memory=False) # by default read first sheet of the file
     if st.button('Find column outliers'):
             boxplotter('labor_duration',True,data=df)
-
-
