074e575d27b0f394950122bd23dc2bcdc3212910
YonatanHanasav1 <149471467+YonatanHanasav1@users.noreply.github.com>
2023-12-19 14:48:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
 import plotly.graph_objects as go
 
 def boxplotter(column_str, field, data):
@@ -15,20 +14,16 @@ def boxplotter(column_str, field, data):
     
     median = round(filtered_data[column_str].median(), 1)
 
-    # Set Seaborn theme (you can choose different themes)
-    sns.set_theme(style="whitegrid")
-
     # Create a boxplot using Plotly's graph_objects
     fig = go.Figure()
 
     # Add the box and whisker plot with only outliers
-    fig.add_trace(go.Box(
+    box_trace = go.Box(
         y=filtered_data[column_str],
         boxpoints="outliers",  # Show only outliers
-        #marker=dict(color='blue'),
-        #line=dict(color='blue'),
         hoverinfo='y+text',  # Display y-axis (box) and text (hovertext)
-    ))
+    )
+    fig.add_trace(box_trace)
 
     # Add a red line for the median
     fig.add_shape(
@@ -51,12 +46,21 @@ def boxplotter(column_str, field, data):
         height=400
     )
 
+    # Count outliers and get the values of the highest 5 outliers
+    outliers = box_trace.boxpoints['outliers'][0]
+    num_outliers = len(outliers)
+    highest_5_outliers = sorted(outliers)[-5:]
+
+    # Display the count of outliers and the values of the highest 5 outliers
+    st.write(f"Number of outliers: {num_outliers}")
+    st.write(f"Highest 5 outliers: {highest_5_outliers}")
+
     # Display the plot using Streamlit's `st.plotly_chart`
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 uploaded_file = st.file_uploader("Choose a file", type=["csv"])
 
 if uploaded_file:
-    df = pd.read_csv(uploaded_file, low_memory=False) # by default read first sheet of the file
+    df = pd.read_csv(uploaded_file, low_memory=False) # by default read the first sheet of the file
     if st.button('Find column outliers'):
         boxplotter('labor_duration', True, data=df)
