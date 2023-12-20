af3816e05f2431554be54afc1820315141b3f57d
YonatanHanasav1 <yonatan.hanasav@aquant.io>
2023-12-20 11:47:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,43 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+import plotly.express as px
 
-def boxplotter(column_str, field, data):
-    if field == 'field':
+def display_dataset(data):
+    st.sidebar.title("Settings")
+    check_box1 = st.sidebar.checkbox(label="Display dataset")
+
+    if check_box1:
+        st.write(data.head())
+
+def format_and_display_table(data, title, column_str, id_column, name_column):
+    formatted_table = data.reset_index(drop=True)
+    formatted_table.index += 1  # Reset index starting from 1
+    formatted_table[column_str] = formatted_table[column_str].astype(str).str.replace(r'[\(\)]', '')
+    formatted_table[id_column] = formatted_table[id_column].astype(str)
+    formatted_table[name_column] = formatted_table[name_column].astype(str)
+
+    if column_str == 'labor_duration':
+        formatted_table = formatted_table[[id_column, column_str]]
+    else:
+        formatted_table = formatted_table[[id_column, name_column, column_str]]
+
+    st.table(formatted_table)
+
+def boxplotter(column_str, data):
+    if column_str == 'field_labor_duration':
+        column_str = 'labor_duration'
         filtered_data = data[data['event_type'] == 'field']
         event_type_label = 'field'
         st.write(f"Analyzing outliers of {event_type_label}_{column_str}:")
-    elif field == 'remote':
+    elif column_str == 'remote_labor_duration':
+        column_str = 'labor_duration'
         filtered_data = data[data['event_type'] == 'remote']
         event_type_label = 'remote'
         st.write(f"Analyzing outliers of {event_type_label}_{column_str}:")
-    elif field == '':
-        filtered_data = data[data['total_part_cost']>0]
+    elif column_str == 'part_cost':
+        column_str = 'total_part_cost'
+        filtered_data = data[data['total_part_cost'] > 0]
         st.write(f"Analyzing outliers of {column_str}:")
 
     # Create a boxplot using Plotly's graph_objects
@@ -32,57 +57,33 @@ def boxplotter(column_str, field, data):
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
     # Get the value of the most extreme outliers
-    highest_5_outliers = filtered_data[[column_str, 'investigation_id','part_name','part_id']].sort_values(by=column_str, ascending=False).head(5)
-    lowest_5_outliers = filtered_data[[column_str, 'investigation_id','part_name','part_id']].sort_values(by=column_str, ascending=True).head(5)
+    highest_5_outliers = filtered_data[[column_str, 'investigation_id', 'part_name', 'part_id']].sort_values(
+        by=column_str, ascending=False).head(5)
+    lowest_5_outliers = filtered_data[[column_str, 'investigation_id', 'part_name', 'part_id']].sort_values(
+        by=column_str, ascending=True).head(5)
 
     # Display the tables side by side with matching headlines
     col1, col2 = st.columns(2)
 
-    if field == 'field' or field == 'remote':
-        with col1:
-            st.write(f"Highest 5 outliers for {event_type_label}_{column_str}:")
-            formatted_highest = highest_5_outliers.reset_index(drop=True)
-            formatted_highest.index += 1  # Reset index starting from 1
-            formatted_highest[column_str] = formatted_highest[column_str].apply(lambda x: str(x).replace('(', '').replace(')', ''))
-            formatted_highest['Investigation ID'] = formatted_highest['investigation_id'].apply(lambda x: str(x))
-            formatted_highest = formatted_highest[['Investigation ID', column_str]]
-            st.table(formatted_highest)
+    with col1:
+        title_text = f"Highest 5 outliers for {event_type_label}_{column_str}:" if column_str == 'labor_duration' else f"Highest 5 outliers for {column_str}:"
+        st.write(title_text)
+        format_and_display_table(highest_5_outliers, title_text, column_str, 'investigation_id', 'part_name')
 
-        with col2:
-            st.write(f"Lowest 5 outliers for {event_type_label}_{column_str}:")
-            formatted_lowest = lowest_5_outliers.reset_index(drop=True)
-            formatted_lowest.index += 1  # Reset index starting from 1
-            formatted_lowest[column_str] = formatted_lowest[column_str].apply(lambda x: str(x).replace('(', '').replace(')', ''))
-            formatted_lowest['Investigation ID'] = formatted_lowest['investigation_id'].apply(lambda x: str(x))
-            formatted_lowest = formatted_lowest[['Investigation ID', column_str]]
-            st.table(formatted_lowest)
-    else:
-        with col1:
-            st.write(f"Highest 5 outliers for {column_str}:")
-            formatted_highest = highest_5_outliers.reset_index(drop=True)
-            formatted_highest.index += 1  # Reset index starting from 1
-            formatted_highest[column_str] = formatted_highest[column_str].apply(lambda x: str(x).replace('(', '').replace(')', ''))
-            formatted_highest['Part ID'] = formatted_highest['part_id'].apply(lambda x: str(x))
-            formatted_highest['Part Description'] = formatted_highest['part_name'].apply(lambda x: str(x))
-            formatted_highest = formatted_highest[['Part ID','Part Description', column_str]]
-            st.table(formatted_highest)
+    with col2:
+        title_text = f"Lowest 5 outliers for {event_type_label}_{column_str}:" if column_str == 'labor_duration' else f"Lowest 5 outliers for {column_str}:"
+        st.write(title_text)
+        format_and_display_table(lowest_5_outliers, title_text, column_str, 'investigation_id', 'part_name')
 
-        with col2:
-            st.write(f"Lowest 5 outliers for {column_str}:")
-            formatted_lowest = lowest_5_outliers.reset_index(drop=True)
-            formatted_lowest.index += 1  # Reset index starting from 1
-            formatted_lowest[column_str] = formatted_lowest[column_str].apply(lambda x: str(x).replace('(', '').replace(')', ''))
-            formatted_lowest['Part ID'] = formatted_lowest['part_id'].apply(lambda x: str(x))
-            formatted_lowest['Part Description'] = formatted_lowest['part_name'].apply(lambda x: str(x))
-            formatted_lowest = formatted_lowest[['Part ID','Part Description', column_str]]
-            st.table(formatted_lowest)
-
-
-uploaded_file = st.file_uploader("Choose a file", type=["csv"])
+columns = ['field_labor_duration', 'remote_labor_duration', 'part_cost']
+uploaded_file = st.file_uploader("Please load sc_events file", type=["csv"])
 
 if uploaded_file:
-    df = pd.read_csv(uploaded_file, low_memory=False) # by default read the first sheet of the file
-    if st.button('Find column outliers'):
-        boxplotter('labor_duration', 'field', data=df)
-        boxplotter('labor_duration', 'remote', data=df)
-        boxplotter('total_part_cost', '', data=df)
+    df = pd.read_csv(uploaded_file, low_memory=False)  # by default read the first sheet of the file
+    display_dataset(df)
+
+    feature_selection = st.sidebar.multiselect(label="Select columns to analyze", options=columns)
+
+    if feature_selection:
+        for col in feature_selection:
+            boxplotter(col, data=df)
